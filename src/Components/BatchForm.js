a0d2d7b4b58e4d1e616ff1e8ab0fe847cd612437
Richard Hill III <rhillz718@gmail.com>
2018-08-15 12:45:17 -0400

    included batch form to mysql db and added state changes

--- src/Components/BatchForm.js
+++ src/Components/BatchForm.js
@@ -18,18 +18,241 @@ class BatchForm extends Component{
         wzToggled: false,
         wzValue: 1,
         prodUseValue: 1,
+        startingMethodVal: null,
+        extractionComplete: false,
+        purgeComplete: false,
+
+        // startingMethod: null,
+        material_amount: null,
+        passes: null,
+        soak_time: null,
+        recovery_time: null,
+        recovery_temp: null,
+        ethanol_amount: null,
+        dry_ice_amount: null,
+        wz_time: null,
+
+        purge_temp: null,
+        flips: null,
+        purge_time: null,
+
+        total_yield: null,
+        carts_produced: null,
+        use_case: null,
     }
 
+//FUCNTION TO HANDLE STARTING METHOD CHANGE
+    handleStartingMethod = (e, i, val) => {this.setState({startingMethodVal: val})}
+///////////////////////////////////////////
 
+//RENDER FORM FOR SELECTED STARTING METHOD
+    renderExtractionForm = () => {
+         if(this.state.startingMethodVal === "BHO"){
+             return (
+                <div>
+                    <TextField 
+                        hintText="Amount of Trim used (oz):" 
+                        type="number"
+                        onChange = {(e)=> this.setState({material_amount: e.target.value})}
+                        />
+                    <br/>
+                    <TextField
+                        hintText="Number of passes:" 
+                        type="number"
+                        onChange = {(e)=> this.setState({passes: e.target.value})}
+                        />
+                     <br/>
+                    <TextField 
+                        hintText="Recovery Time(mins):" 
+                        type="number"
+                        onChange={(e)=> this.setState({recovery_time: e.target.value})}
+                        />
+                     <br/>
+                    <TextField 
+                        hintText="Recovery Temperature(f):" 
+                        type="number"
+                        onChange = {(e)=> this.setState({recovery_temp: e.target.value})}
+                        />
+                      <br/>
+                    <TextField 
+                        hintText="Amount of Dry Ice:" 
+                        type="number" 
+                        onChange = {(e)=>this.setState({dry_ice_amount: e.target.value})}
+                        />
+                </div>
+                );
+         } else if (this.state.startingMethodVal === "QWET"){
+             return (
+                <div>
+                    <TextField 
+                        hintText="Amount of Trim used (oz):" 
+                        type="number"
+                        onChange = {(e)=> this.setState({material_amount: e.target.value})}
+                        />
+                    <br/>
+                     <TextField 
+                        hintText="Amount of Ethanol(oz):" 
+                        type="number"
+                        onChange={(e)=>this.setState({ethanol_amount: e.target.value})}
+                        />
+                    <br/>
+                    <TextField 
+                        hintText="Soak Time:" 
+                        type="number"
+                        onChange = {(e)=>this.setState({soak_time: e.target.value})}
+                        />
+                     <br/>
+                     <TextField 
+                        hintText="Recovery Time(mins):" 
+                        type="number"
+                        onChange={(e)=> this.setState({recovery_time: e.target.value})}
+                        />
+                     <br/>
+                    <TextField 
+                        hintText="Recovery Temperature(f):" 
+                        type="number"
+                        onChange = {(e)=> this.setState({recovery_temp: e.target.value})}
+                        />
+                      <br/>
+                    <TextField 
+                        hintText="Amount of Dry Ice:" 
+                        type="number" 
+                        onChange = {(e)=>this.setState({dry_ice_amount: e.target.value})}
+                        />
+                </div>
+                )
+         } else {
+             return <span className="extractionText"> Please select a method.</span>
+         }
+             
+         
+     };
+//////////////////////////////////////////
+
+//RENDER WINTERIZATION TOGGLE FOR BHO
+     renderWzToggle = () => {
+         if(this.state.startingMethod === 1){
+         return  <Toggle
+                    label="Winterized?"
+                    onToggle={this.handleWzToggle}
+                    toggled={this.state.wzToggled}
+
+                /> 
+         }
+     };
+
+
+/////////////////////////////////////
+
+// RENDER PURGE PART OF FORM
+    renderPurgeForm = () => {
+         if(this.state.extractionComplete){
+             return (
+                 <div>
+                 <h3> Purging Process </h3>
+        
+                    <TextField
+                        hintText="Purge Temperture(f):"
+                        type="number"
+                        onChange={(e)=>this.setState({purge_temp: e.target.value})}
+                    />
+                     <br/>
+                    <TextField
+                        hintText="Amount of times Flipped:"
+                        type="number"
+                        onChange={(e)=>this.setState({flips: e.target.value})}
+
+                    />
+                     <br/>
+                    <TextField
+                        hintText="Time Purged(hours):"
+                        type="number"
+                        onChange={(e)=> this.setState({purge_time: e.target.value})}
+                    />
+                    <br/>
+                    <RaisedButton
+                        label="Purge Complete"
+                        fullWidth={true}
+                        backgroundColor='#fff200'
+                        onClick={this.completePurge}
+                    />
+                    <Divider/>
+                </div>
+             )
+         }
+     };
+
+/////////////////////////////
+
+//RENDER PRODUCTION FORM
+    renderProductionForm = () => {
+         if(this.state.purgeComplete){
+             return(
+                <div>
+                    <h3>Production Process</h3>
+
+                    <TextField
+                        hintText="Total Yield(gm):"
+                        type="number"
+                        onChange={(e)=>this.setState({total_yield: e.target.value})}
+                    />
+                    < br/>
+                    <SelectField
+                                floatingLabelText="Used For:"
+                                value={this.state.prodUseValue}
+                                onChange={this.handleProdUseValueOnChange}
+                            >
+                                <MenuItem value={1} primaryText="Oil" />
+                                <MenuItem value={2} primaryText="Shatter" />
+                                <MenuItem value={3} primaryText="Wax"/>
+                            </SelectField>
+                            <br/>
+                    {this.renderOilOptions()}
+                    <Divider/>
+                </div>
+             )
+        }
+     }
+////////////////////////
+
+//FUNCTION WHEN EXTRACTION IS COMPLETE
+    completeExtraction = () => {this.setState({extractionComplete:true})}
+
+/////////////////////////////////////
+
+//FUCNTION WHEN PURGE IS COMPLETE
+    completePurge = () => {this.setState({purgeComplete: true})}
+/////////////////////////////////
+
+//RENDER BATCH COMPLETE BUTTON
+    renderBatchCompleteBtn = () => {
+         if(this.state.purgeComplete){
+            return (
+                <RaisedButton
+                    label="Batch Complete"
+                    fullWidth={true}
+                    backgroundColor="#ff0000"
+                    />
+            )
+         }
+     }
+
+/////////////////////////////
+
+//HANDLE WZ VALUE CHANGE
     handleWzValueOnChange = (event, index, value) =>{
         this.setState({wzValue: value})
     }
+////////////////////////
 
-    handleProdUseValueOnChange = (event, index, value)=>{
-        this.setState({prodUseValue: value})
+
+//HANDLE  PRODUCT USE VALUE  
+    handleProdUseValueOnChange = (e, index, value)=>{
+        this.setState({prodUseValue: value, use_case: e.target.value})
     }
+/////////////////////////
 
-
+//
     handleToggle=()=>{
         if(!this.state.dewaxToggled){
             this.setState({dewaxToggled: true})
@@ -49,7 +272,7 @@ class BatchForm extends Component{
            return (
                <div>
                <TextField hintText="Time Dewaxed (mins):" type="number"/>
-               <br/>
+               br//>
                </div>
             );
        }
@@ -62,7 +285,7 @@ class BatchForm extends Component{
                 <TextField
                     hintText="Time Winterized:"
                     />
-                    <br/>
+                   < br/>
                 <SelectField
                     floatingLabelText="Method Used"
                     value={this.state.wzValue}
@@ -93,14 +316,11 @@ class BatchForm extends Component{
 
     render(){
         const idVal = "ID Number: "+" " + 0
-
-
         console.log(this.state)
-        console.log(this.props)
         return(
 
         <Dialog
-            title="New Batch"
+            title="Batch Form"
             open={this.props.open}
             autoScrollBodyContent={true}
             onRequestClose={this.props.close}
@@ -110,118 +330,44 @@ class BatchForm extends Component{
         </div>
   
         <h3>Extracting Process</h3>
-        <TextField
-            name="ID"
-            disabled
-            id="0"
-            // type="number"
-            value={idVal}
-        />
-        <DatePicker
-            hintText="Start-Date"
-            />
-
-        <TextField
-            hintText="Amount of Trim used (oz):"
-            type="number"
-            />
-        <TextField
-            hintText="Number of passes:"
-            type="number"
-            />
-        <TextField
-            hintText="Recovery Time(mins):"
-            type="number"
-            />
-        <TextField
-            hintText="Recovery Temperature(f):"
-            type="number"
-            />
-        <Toggle
-            label="Dewaxed?"
-            onToggle={this.handleToggle}
-            toggled={this.state.dewaxToggled}
-
-            />
-
-            {this.renderDewaxTime()}
-
-        
-        <Divider/>
-        <h3> Purging Process </h3>
-        <TextField
-            hintText="Amount of ethanol(oz):"
-            type="number"
-        />
-        <Toggle
-            label="Winterized?"
-            onToggle={this.handleWzToggle}
-            toggled={this.state.wzToggled}
-
-        />
-        {this.renderWzOptions()}
-        <TextField
-            hintText="Purge Temperture(f):"
-            type="number"
-        />
-        <TextField
-            hintText="Days Purged:"
-            type="number"
-        />
-
-        <TextField
-            hintText="Amount of times Flipped:"
-            type="number"
-        />
-        <TextField
-            hintText="Time Purged(hours):"
-            type="number"
-        />
-        <DatePicker
-            hintText="Date Completed"
-        />
-        <Divider/>
-        <h3>Production Process</h3>
-
-        <TextField
-            hintText="Total Yield(gm):"
-            type="number"
-        />
-        <br/>
+            
         <SelectField
-                    floatingLabelText="Used For:"
-                    value={this.state.prodUseValue}
-                    onChange={this.handleProdUseValueOnChange}
-                >
-                    <MenuItem value={1} primaryText="Oil" />
-                    <MenuItem value={2} primaryText="Shatter" />
-                    <MenuItem value={3} primaryText="Wax"/>
-                </SelectField>
-                <br/>
-        {this.renderOilOptions()}
+            floatingLabelText="Select starting method"
+            value={this.state.startingMethodVal}
+            onChange={this.handleStartingMethod}
+            >
+                <MenuItem value={"BHO"} primaryText="BHO"/>
+                <MenuItem value={"QWET"} primaryText="QWET"/>
+        </SelectField>
+
+        <br/>
+
+        {this.renderExtractionForm()}
+        {this.renderWzToggle()}
+        {this.renderWzOptions()}
+        <br/>
+        <RaisedButton 
+            label="Extraction Complete" 
+            fullWidth={true} 
+            backgroundColor='#3fff00'
+            onClick={this.completeExtraction}
+        />
         <Divider/>
+        {this.renderPurgeForm()}
+        {this.renderProductionForm()}
+        
+        
         <h3>Additional Notes</h3>
             <TextField
                 hintText="notes..."
                 multiLine={true}
                 rows={5}
                 rowsMax={5}
-                />
-                <br/>
+            />
+            <br/>
 
-        <RaisedButton>Complete</RaisedButton>
-
-
-
-      
-
-
-
-
-
-
-            
-        </Dialog>
+        {this.renderBatchCompleteBtn()}
+    </Dialog>
         )
     }
 }
